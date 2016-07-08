e441fbe62a3b6a5e9e7055df222486bdf9a3c7c0
zoharsanchez <piccola.lupina@gmail.com>
2016-07-08 16:30:10 -0700

    Have templates for links and shorten

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,9 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {};
+
+  Links.getAll().then(function(response) {
+    $scope.data.links = response;
+  });
 });
