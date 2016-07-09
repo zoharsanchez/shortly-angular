1b081b48d12335616bf10136d1d0fc6df5166854
zoharsanchez <piccola.lupina@gmail.com>
2016-07-08 20:44:03 -0700

    Remove comment

--- client/app/app.js
+++ client/app/app.js
@@ -23,7 +23,6 @@ angular.module('shortly', [
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
     });
-    // Your code here
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
